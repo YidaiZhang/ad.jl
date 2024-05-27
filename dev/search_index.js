176e54950f71349ed48a90fec51d065056089955
Documenter.jl <documenter@juliadocs.github.io>
2024-05-27 06:31:44 +0000

    build based on 595e1cd

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ad","category":"page"},{"location":"#ad","page":"Home","title":"ad","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ad.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ad]","category":"page"}]
+}
